1e62778c1340b66f37e6bde058e036e47b16d9cb
Joshua Steed <joshuasteed123@gmail.com>
2022-08-23 11:46:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "welcome to my database homepage",
+  });
+});
+
+const route = require("./routes/route");
+app.use("/users", route);
+
+app.listen(app.get("port"), () => {
+  console.log(`Listening for calls on http://localhost:${app.get("port")}`);
+  console.log("Press Ctrl+C to exit the server");
+});
