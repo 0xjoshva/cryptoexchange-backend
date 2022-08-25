be650b8565ff71a98e16bd2a6f1ebeb6a39aa294
Joshua Steed <joshuasteed123@gmail.com>
2022-08-25 16:09:41 +0200

    changes

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -17,7 +17,20 @@ router.get("/", (req, res) => {
 });
 
 // show transactions for specific user id
-
+router.get("/:id", (req, res) => {
+  try {
+    con.query(
+      `SELECT * FROM transactions WHERE user_id = ${req.params.id}`,
+      (err, result) => {
+        if (err) throw err;
+        res.json(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
 
 
 
