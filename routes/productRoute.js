16628949e71fe9bb37784018d7651e5ad1ad6e80
Joshua Steed <joshuasteed123@gmail.com>
2022-09-08 08:48:51 +0200

    crypto single view

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -15,6 +15,19 @@ router.get("/", (req, res) => {
   }
 });
 
-
+router.get("/:id", (req, res) => {
+  try {
+    con.query(
+      `SELECT * FROM cryptos WHERE crypto_id = ${req.params.id}`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
 
 module.exports = router;
